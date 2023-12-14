1e3f834bc5dbabbd09d4d0edde73164809bfdd15
ihirakiwa <killiann76170@gmail.com>
2023-12-14 18:17:21 +0100

    all

--- projet/projet.py
+++ projet/projet.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import random
-
+import numpy as np
 
 # -----------------------------------------------------------------------------
 # Graph tools
@@ -16,6 +16,7 @@ def matrice_adjacence_to_liste_adjacence(matrice):
         liste_adjacence[i] = voisins
     return liste_adjacence
 
+
 def liste_adjacence_to_matrice_adjacence(liste_adjacence):
     # Trouver le nombre de sommets dans le graphe
     nb_sommets = (
@@ -29,15 +30,14 @@ def liste_adjacence_to_matrice_adjacence(liste_adjacence):
         + 1
     )
 
-    # Initialiser une matrice d'adjacence remplie de zéros
     matrice_adjacence = [[0] * nb_sommets for _ in range(nb_sommets)]
 
-    # Remplir la matrice d'adjacence en fonction de la liste d'adjacence
     for sommet, voisins in liste_adjacence.items():
         for voisin in voisins:
             matrice_adjacence[sommet][voisin] = 1
 
     return matrice_adjacence
+
 
 def visualiser_graphe(graph, coloration):
     # create an graph
@@ -64,14 +64,14 @@ def visualiser_graphe(graph, coloration):
 
 # Exemple of adjacence list
 graph_matrice_adjacence = [
-    [0, 0, 0, 1, 0, 1, 0,1],
-    [0, 0, 1, 0, 1, 0, 1,0],
-    [0, 1, 0, 0, 0, 1, 0,1],
-    [1, 0, 0, 0, 1, 0, 1,0],
-    [0, 1, 0, 1, 0, 0, 0,1],
-    [1, 0, 1, 0, 0, 0, 1,0],
-    [0, 1, 0, 1, 0, 1, 0,0],
-    [1, 0, 1, 0, 1, 0, 0,0]
+    [0, 0, 0, 1, 0, 1, 0, 1],
+    [0, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 0, 0, 1],
+    [1, 0, 1, 0, 0, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 0],
+    [1, 0, 1, 0, 1, 0, 0, 0],
 ]
 
 # Convert the matrice to an adjacence list
@@ -194,39 +194,40 @@ visualiser_graphe(graph_liste_adjacence, coloration_welsh)
 # algorithm for graph coloring
 # -----------------------------------------------------------------------------
 
-class Graph():
- 
+
+class Graph:
     def __init__(self, vertices):
         self.V = vertices
-        self.graph = [[0 for column in range(vertices)]
-                      for row in range(vertices)]
- 
+        self.graph = [[0 for column in range(vertices)] for row in range(vertices)]
+
     # A utility function to check
     # if the current color assignment
     # is safe for vertex v
     def isSafe(self, v, colour, c):
         for i in range(self.V):
-            if (self.graph[v][i] == 1 and colour[i] == c) or (self.graph[i][v] == 1 and colour[i] == c):
+            if (self.graph[v][i] == 1 and colour[i] == c) or (
+                self.graph[i][v] == 1 and colour[i] == c
+            ):
                 return False
         return True
 
     def add_edge(self, u, v):
         self.graph[u][v] = 1
         self.graph[v][u] = 1
-        
+
     # A recursive utility function to solve m
     # coloring problem
     def graphColourUtil(self, m, colour, v):
         if v == self.V:
             return True
- 
+
         for c in range(1, m + 1):
             if self.isSafe(v, colour, c) == True:
                 colour[v] = c
                 if self.graphColourUtil(m, colour, v + 1) == True:
                     return True
                 colour[v] = 0
- 
+
     def graphColouring(self):
         m = 1
         colour = [0] * self.V
@@ -234,7 +235,8 @@ class Graph():
             m += 1
             colour = [0] * self.V
         return colour
-    
+
+
 """
 g = Graph(len(graph_matrice_adjacence))
 g.graph = graph_matrice_adjacence
@@ -244,8 +246,6 @@ visualiser_graphe(matrice_adjacence_to_liste_adjacence(g.graph),g.graphColouring
 # To design and implement in Python a coloring algorithm for the case
 # of a dynamic graph where the number of vertices and edges evolve over time.
 # -----------------------------------------------------------------------------
-
-
 
 
 def remove_node(graph_liste_adjacence, noeud):
@@ -301,15 +301,19 @@ def modify_graph_dynamically(liste_adjacence):
         j = random.randint(0, list(liste_adjacence.keys())[-1])
         while j not in liste_adjacence:
             j = random.randint(0, list(liste_adjacence.keys())[-1])
-        if i != j :
+        if i != j:
             if j not in liste_adjacence[i]:
                 print(f"Adding link between the node {i} and the node {j}.")
                 liste_adjacence[i].append(j)
                 liste_adjacence[j].append(i)
-            else :
-                print(f"cant add the link because those random numbers {i},{j} cant be used\n")
+            else:
+                print(
+                    f"cant add the link because those random numbers {i},{j} cant be used\n"
+                )
         else:
-            print(f"cant add the link because those random numbers {i},{j} cant be used\n")
+            print(
+                f"cant add the link because those random numbers {i},{j} cant be used\n"
+            )
 
     elif operation == 3:  # remove an edge
         i = random.randint(0, list(liste_adjacence.keys())[-1])
@@ -355,8 +359,7 @@ def observe_graph_evolution(initial_graph, num_iterations):
         g = Graph(len(graph_matrice_adjacence))
         g.graph = graph_matrice_adjacence
         visualiser_graphe(current_graph, g.graphColouring())
-        
-        
+
 
 """
 observe_graph_evolution(graph_liste_adjacence, 4)
@@ -378,7 +381,7 @@ def get_sudoku_grid():
 
 
 def display_4x4_sudoku(grid):
-    print("+----+----+")
+    print("+-----+-----+")
     for i in range(4):
         for j in range(4):
             if j % 2 == 0:
@@ -389,89 +392,62 @@ def display_4x4_sudoku(grid):
                 print(grid[i][j], end=" ")
         print("|")
         if (i + 1) % 2 == 0:
-            print("+----+----+")
+            print("+-----+-----+")
 
 
-sudoku_4x4 = [
-    [1, 0, 0, 0],
-    [0, 2, 0, 0],
-    [0, 0, 3, 0],
-    [0, 0, 0, 4]
-]
+sudoku_4x4 = [[1, 0, 0, 0], [0, 2, 0, 0], [0, 0, 3, 0], [0, 0, 0, 4]]
 
-def sudoku_to_adjacency_matrix(sudoku):
-    n = 4  # For a 4x4 Sudoku
-    adjacency_matrix = [[0] * (n * n) for _ in range(n * n)]
+
+def sudoku_to_adjacency_list(sudoku):
+    n = len(sudoku)
+    adjacency_list = {i: set() for i in range(n * n)}
 
     for i in range(n):
         for j in range(n):
-            if sudoku[i][j] != 0:
-                for k in range(n):
-                    if k != j:
-                        adjacency_matrix[i * n + j][i * n + k] = 1  # same row
-                    if k != i:
-                        adjacency_matrix[i * n + j][k * n + j] = 1  # same column
-                subgrid_row = 2 * (i // 2)
-                subgrid_col = 2 * (j // 2)
-                for x in range(2):
-                    for y in range(2):
-                        if x != i % 2 or y != j % 2:
-                            adjacency_matrix[i * n + j][(subgrid_row + x) * n + subgrid_col + y] = 1  # same subgrid
+            # Same row
+            for k in range(n):
+                if k != j:
+                    adjacency_list[n * i + j].add(n * i + k)
 
-    # Add numbers of Sudoku to the diagonal
-    for i in range(n * n):
-        adjacency_matrix[i][i] = sudoku[i // n][i % n]
+            # Same column
+            for k in range(n):
+                if k != i:
+                    adjacency_list[n * i + j].add(n * k + j)
 
-    return adjacency_matrix
+            # Same 2x2 sub-grid
+            start_row, start_col = i - i % 2, j - j % 2
+            for x in range(2):
+                for y in range(2):
+                    if start_row + x != i or start_col + y != j:
+                        adjacency_list[n * i + j].add(
+                            n * (start_row + x) + start_col + y
+                        )
+
+    # Convert sets to lists for consistency with the original function
+    adjacency_list = {key: list(value) for key, value in adjacency_list.items()}
+
+    return adjacency_list
 
 
-def apply_coloring_to_sudoku(sudoku, coloring):
-    n = 4  # For a 4x4 Sudoku
+def sudoku_to_value_list(sudoku):
+    n = len(sudoku)
+    value_list = [0] * (n * n)
+
     for i in range(n):
         for j in range(n):
-            if sudoku[i][j] == 0:
-                sudoku[i][j] = coloring[i * n + j]
+            value_list[n * i + j] = sudoku[i][j]
 
-    return sudoku
+    return value_list
 
 
-def display_4x4_sudoku(grid):
-    print("+----+----+")
-    for i in range(4):
-        for j in range(4):
-            if j % 2 == 0:
-                print("|", end=" ")
-            if grid[i][j] == 0:
-                print(".", end=" ")
-            else:
-                print(grid[i][j], end=" ")
-        print("|")
-        if (i + 1) % 2 == 0:
-            print("+----+----+")
-
-"""
-# Example Sudoku
-sudoku_4x4 = [
-    [1, 0, 0, 0],
-    [0, 2, 0, 0],
-    [0, 0, 3, 0],
-    [0, 0, 0, 4]
-]
-
-# Convert Sudoku to adjacency matrix
-adjacency_matrix = sudoku_to_adjacency_matrix(sudoku_4x4)
-
-# Use graph coloring algorithm
-num_colors = 4  # As it's a 4x4 Sudoku
-g = Graph(len(adjacency_matrix))
-g.graph = adjacency_matrix
-coloring = g.graphColouring(num_colors)
-
-# Apply coloring to Sudoku
-solved_sudoku_4x4 = apply_coloring_to_sudoku(sudoku_4x4, coloring)
-
-# Display the results
+value_list = sudoku_to_value_list(sudoku_4x4)
+print(sudoku_to_adjacency_list(sudoku_4x4))
+print("#----------------------#")
+print(value_list)
 print("Original Sudoku:")
 display_4x4_sudoku(sudoku_4x4)
-print("\nSolved Sudoku:")
-display_4x4_sudoku(solved_sudoku_4x4)"""
+
+
+visualiser_graphe(
+    sudoku_to_adjacency_list(sudoku_4x4), sudoku_to_value_list(sudoku_4x4)
+)
